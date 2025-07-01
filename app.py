22079d0da582f82b91a317c1ef1b29a506937881
Shreya <56733647+shreyaviq@users.noreply.github.com>
2025-07-01 12:59:53 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 import base64
 import os
 
-st.set_page_config(layout="wide", page_title="HRSC-DaSH", page_icon="🚰")
+st.set_page_config(layout="wide", page_title="MVS", page_icon="🚰")
 
 # Remove top padding to reduce white space
 st.markdown("""
@@ -63,7 +63,7 @@ if selected == "Home":
     with col2:
         st.markdown("""
         ### Purpose  
-        Enable governed HR data migration from SAP HCM to SuccessFactors:  
+        Enable governed HR data migration from Legacy HR Systems to SuccessFactors and S4HANA:  
         - Schema mapping & transformation engine  
         - Pre-load validation, rollback, and licensing  
         - Audit-ready configurations for scale  
@@ -76,7 +76,7 @@ if selected == "Home":
         """)
 
     st.markdown("### Accelerate Your SAP Employee Central Migration")
-    st.markdown("#### Purpose-built migration, validation and discrepancy analysis to make your SAP HCM transformation effortless.")
+    st.markdown("#### Purpose-built migration, validation and discrepancy analysis to make your HR Data Migration and Payroll effortless.")
 
     icons_data = [
         ("streamline.png", "Streamline Your SAP HCM Migration"),
@@ -145,6 +145,7 @@ if selected == "Home":
     <div style='background-color:#002b5c;padding:40px;margin-top:50px;border-radius:10px;'>
         <h3 style='color:white;text-align:center;'>Built for SAP & SuccessFactors</h3>
         <p style='color:white;text-align:center;'>Our platform is designed to simplify, safeguard, and speed up your transformation journey.</p>
+        <p style='color:white;text-align:center;'>Upgrading the tool to include all feature releases.</p>
         <div style='display:flex;justify-content:space-around;margin-top:30px;'>
             <div style='width:30%;text-align:center;'>
                 <h4 style='color:white;'>Data Migration Made Easy</h4>
@@ -230,7 +231,7 @@ elif selected == "Solutions":
 
 # --- SERVICES PAGE ---
 elif selected == "Services":
-    st.markdown("## End-to-End SAP HCM Migration Services")
+    st.markdown("## End-to-End SAP HCM and SuccessFactors Migration Services")
     st.markdown(
         """
         Whether you are migrating to Employee Central or optimizing your existing setup, our services are tailored to simplify your journey:
